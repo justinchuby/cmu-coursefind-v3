94c6a94b533f1885b4528f4a79a7d756e679f0be
justinchuby <justinchuby@users.noreply.github.com>
2020-07-25 17:24:21 -0400

    First commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,51 @@
+<svelte:head>
+  <title>About - CMU Course Find</title>
+</svelte:head>
+
+<h1>About CMU Course Find</h1>
+
+<p>
+  CMU Course Find is a 15-112 term project by Justin Chu (me) during the Fall
+  2015 semester at Carnegie Mellon University. It has been updated over the
+  years.
+</p>
+<p>
+  I had this idea one day when I was wandering in Wean Hall. I saw students
+  having class and hoped to find out what class they were having. I could just
+  walk in and see what was going on, but it would be much better if I knew
+  something about the class.
+</p>
+<p>
+  CMU Course Find makes it easier for you to figure out what’s going on in a
+  class room. You can also see
+  <b>what’s happening</b>
+  at a particular time, so you can
+  <b>go and discover</b>
+  courses that interest you. This web app is also great for finding the room for
+  a class, or just browsing the catalog to see what’s interesting.
+</p>
+<p>
+  To find a course, you may search for the name of a course, name of
+  instructors, name of a building, number of a course, or a room number.
+</p>
+<p>
+  If you have any suggestion or if you would like to contribute to this project,
+  please don’t hesitate to submit a feedback!
+</p>
+<p>
+  <br />
+  Happy learning!
+  <br />
+  Justin
+</p>
+<p>
+  <br />
+  <br />
+  - Watch the introduction video on
+  <a
+    href="https://youtu.be/QzxAUsKCtxM"
+    target="_blank"
+    rel="nofollow noopener noreferrer">
+    Youtube
+  </a>
+</p>
---
+++ src/routes/courses/[courseId].svelte
@@ -0,0 +1,101 @@
+<script context="module">
+  // the (optional) preload function takes a
+  // `{ path, params, query }` object and turns it into
+  // the data we need to render the page
+  import { gql } from "apollo-boost";
+  import { query } from "svelte-apollo";
+  import { client } from "../../data";
+  export async function preload({ params }) {
+    const { courseId } = params;
+    console.log(courseId);
+    const GET_COURSE = gql`
+    {
+      courses(filter: {
+        courseId: "${courseId}"
+      }) {
+        name
+        courseId
+        rundate
+        coreqs
+        prereqs
+        desc
+        notes
+        department
+        units
+        prereqsObj {
+          invert
+          reqs
+        }
+        coreqsObj {
+          invert
+          reqs
+        }
+        meetings {
+          name
+          instructors
+          times {
+            building
+            room
+            location
+          }
+        }
+        rundate
+        semester
+        year
+      }
+    }
+    `;
+    const course = query(client, { query: GET_COURSE });
+    return { course };
+  }
+</script>
+
+<script>
+  export let course;
+</script>
+
+<!-- <svelte:head>
+  <title>{course.courseid} - CMU Course Find</title>
+</svelte:head> -->
+
+{#await $course then result}
+<!-- Take the first course -->
+  {#each result.data.courses.slice(0, 1) as course}
+    <h1>{course.courseId}</h1>
+    <h2>{course.name}</h2>
+    <h3>
+      {course.semester.charAt(0).toUpperCase() + course.semester.slice(1)}
+      {course.year}
+      <br />
+      {course.department}
+    </h3>
+    <p>
+      Pre-requisites: {course.prereqs || 'None'}
+      <br />
+      Co-requisites: {course.coreqs || 'None'}
+      <br />
+      Units: {course.units || 'Unknown'}
+      <br />
+    </p>
+
+    <h3>Description</h3>
+    <p>{course.desc}</p>
+
+    <h3>Notes</h3>
+    <p>{course.notes.replace(/\r?\n|\r|[ ]{2,}/g, '')}</p>
+
+    <h3>Meetings</h3>
+    {#if course.meetings}
+      {#each course.meetings as meeting}
+        <p>
+          {meeting.name}
+          <br />
+          {meeting.instructors.join('; ')}
+        </p>
+      {/each}
+    {/if}
+  {/each}
+
+{:catch error}
+  ERROR: {error}
+{/await}
