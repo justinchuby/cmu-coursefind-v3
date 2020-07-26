3535736180062d7880849cf8d72e2c344393a776
justinchuby <justinchuby@users.noreply.github.com>
2020-07-25 20:21:15 -0400

    Eslint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,15 @@
 <style>
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 </style>
 
 <svelte:head>
-	<title>CMU Course Find</title>
+  <title>CMU Course Find</title>
 </svelte:head>
 
 <h1>CMU Course Find</h1>
 
-<h2>Under construction!</h2> 
+<h2>Under construction!</h2>
