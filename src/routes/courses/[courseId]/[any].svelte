7e15663ea70dbfdd503beed286f7f12a71df38fe
justinchuby <justinchuby@users.noreply.github.com>
2020-07-26 06:36:52 -0400

    Fix redirect

--- src/routes/courses/[courseId]/[any].svelte
+++
@@ -1,7 +0,0 @@
-<script context="module">
-    export async function preload({ params }) {
-        console.log(params)
-        const { courseId } = params;
-        return this.redirect(302, `/courses/${courseId}`);
-    }
-</script>
